cbead85936938a53ff23ca988c455b1ff1f94ec5
Mag1str02 <43681246+Mag1str02@users.noreply.github.com>
2019-03-02 11:02:31 +0300

    Caluclator
    calc

---
+++ Controller.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField inputTextField;
+
+    @FXML
+    private Label label1;
+    
+    @FXML
+    private Label label2;
+
+    public void calc(ActionEvent event) {
+        try {
+            //Попробуй тут
+            String number = inputTextField.getText();
+            int n = Integer.parseInt(number);
+            String x16 = Integer.toHexString(n);
+            String x8= Integer.toOctalString(n);
+            label1.setText(x16);
+            label2.setText(x8);
+        } catch (Exception e) {
+      
+        }
+    }
+
+  
+    }
+
